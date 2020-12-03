3357367c7883a076ecb56a2f8231107794172477
Antonitor <toni@jacdemanec.com>
2020-12-03 12:12:41 +0100

    installation and articles

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e95d6a8fe3875dfb569c349dae441d11'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '46bdc49b-222c-4b31-98aa-270869e43ca0'
+};
